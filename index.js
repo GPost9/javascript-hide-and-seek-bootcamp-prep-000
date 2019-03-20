6b639359249e2745882178d496a09b6de27e47b6
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-03-20 16:42:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  var rank = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  var node = document.querySelector('#grand-node');
+  var child = node.children[0];
+  while (child) {
+    node = child;
+    child = node.children[0];
+    
+  }
+  return node;
+}
